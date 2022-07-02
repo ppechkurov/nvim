c9683cc23de0341f5a763e8404c8a6ea0f0c44ed
petr-pechkurov <peter.pechkurov@gmail.com>
2022-07-02 22:49:41 +0300

    Changed comment

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -88,8 +88,8 @@ end
 M.on_attach = function(client, bufnr)
   if client.name == "tsserver" then
     client.server_capabilities.document_formatting = false
-    -- occurences highlighting
   end
+  -- occurences highlighting
   client.server_capabilities.document_highlight = true
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
