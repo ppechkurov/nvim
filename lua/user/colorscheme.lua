6e21a8ee28eb2578b74df69eaa630089753b6a8a
petr-pechkurov <peter.pechkurov@gmail.com>
2022-07-02 18:02:20 +0300

    Installed gruvbox colorscheme

---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,8 @@
+local colorscheme = "gruvbox-material"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+
+if not status_ok then
+  vim.notify("Colorscheme " .. colorscheme .. " not found!")
+  return
+end
