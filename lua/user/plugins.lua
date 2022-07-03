457351af3fc866b41e704c97c7da991f8ba04cb3
petr-pechkurov <peter.pechkurov@gmail.com>
2022-07-03 16:52:25 +0300

    Configured toggle terminal

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -96,6 +96,9 @@ return packer.startup(function(use)
   -- Git
   use("lewis6991/gitsigns.nvim")
 
+  -- Toggle terminal
+  use("akinsho/toggleterm.nvim")
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
