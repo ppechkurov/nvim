4ad49f8700572b0d6a09e03c180be8d9f3f3cb90
Petr Pechkurov <petr.pechkurov@gmail.com>
2022-09-26 13:57:30 +0300

    session hooks

--- lua/user/persisted.lua
+++ lua/user/persisted.lua
@@ -1,9 +1,15 @@
 local status_ok, persisted = pcall(require, "persisted")
 if not status_ok then
-  return
+	return
 end
 
 persisted.setup({
-  autoload = true,
-  allowed_dirs = { "~/.config/", "~/projects/" },
+	autoload = true,
+	allowed_dirs = { "~/.config/", "~/projects/" },
+	before_save = function()
+		pcall(vim.cmd, "NvimTreeClose")
+	end,
+	before_source = function()
+		pcall(vim.cmd, "Alpha")
+	end,
 })
